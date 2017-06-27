1ae3f020aace850b449f4f8e75ee41247b813b68
Aaron Schlesinger <aaschles@microsoft.com>
2017-06-27 16:15:08 -0700

    adding conversions for user and tests with underscore

--- config_test.ts
+++ config_test.ts
@@ -12,12 +12,28 @@ describe("KubeConfig", () => {
         it("should load the kubeconfig file properly", () => {
             let kc = new KubeConfig();
             kc.loadFromFile(kcFileName);
+
+            // check clusters
             expect(kc.clusters.length).to.equal(2);
             let cluster1 = kc.clusters[0];
+            let cluster2 = kc.clusters[1];
             expect(cluster1.name).to.equal("cluster1");
             expect(cluster1.caData).to.equal("CADATA");
             expect(cluster1.server).to.equal("http://example.com");
-            // expect(kc.clusters('cluster2']).to.not.be.null;
+            expect(cluster2.name).to.equal("cluster2");
+            expect(cluster2.caData).to.equal("CADATA");
+            expect(cluster2.server).to.equal("http://example.com");
+
+            // check users
+            expect(kc.users.length).to.equal(2);
+            let user1 = kc.users[0];
+            let user2 = kc.users[1];
+            expect(user1.name).to.equal("user1");
+            expect(user1.certData).to.equal("CADATA");
+            expect(user1.keyData).to.equal("CKDATA");
+            expect(user2.name).to.equal("user2");
+            expect(user2.certData).to.equal("CADATA");
+            expect(user2.keyData).to.equal("CKDATA");
         });
         it("should fail to load a missing kubeconfig file", () => {
             // TODO: make the error check work
--- config_types.ts
+++ config_types.ts
@@ -1,3 +1,5 @@
+import * as u from 'underscore';
+
 export interface Cluster {
     readonly name: string;
     readonly caData: string;
@@ -10,32 +12,57 @@ export function newClusters(a: any): Cluster[] {
     for (let i = 0; i < len; i++) {
         let elt = a[i];
         if (!elt['name']) {
-            throw new Error("'name' missing");
+            throw new Error(`clusters${i}.name is missing`);
         }
         if (!elt.cluster['certificate-authority-data']) {
-            throw new Error("'certificate-authority-data' missing");
+            throw new Error(`clusters[${i}].cluster.certificate-authority-data is missing`);
         }
         if (!elt.cluster['server']) {
-            throw new Error("'server' missing");
+            throw new Error(`clusters[${i}].cluster.server is missing`);
         }
-        console.log("pusing a cluster");
         ret.push({
             name: elt['name'],
             caData: elt.cluster['certificate-authority-data'],
             server: elt.cluster['server']
         });
     }
-    console.log(`returning ${ret.length} clusters`);
     return ret
 }
 
-export interface User { }
-
-export function newUsers(a: any): User[] {
-    return Array<User>();
+export interface User {
+    readonly name: string
+    readonly certData: string
+    readonly keyData: string
 }
 
-export interface Context { }
+export function newUsers(a: any): User[] {
+    return u.map(a, userIterator());
+}
+
+function userIterator(): u.ListIterator<any, User> {
+    return function (elt: any, i: number, list: u.List<any>): User {
+        if (!elt.name) {
+            throw new Error(`contexts[${i}].name is missing`);
+        }
+        if (!elt.user["client-certificate-data"]) {
+            throw new Error(`contexts[${i}].user.client-certificate-data is missing`);
+        }
+        if (!elt.user["client-key-data"]) {
+            throw new Error(`contexts[${i}].user.client-key-data is missing`);
+        }
+        return {
+            name: elt.name,
+            certData: elt.user["client-certificate-data"],
+            keyData: elt.user["client-key-data"]
+        }
+    }
+}
+
+export interface Context {
+    readonly cluster: string
+    readonly user: string
+    readonly name: string
+}
 
 export function newContexts(a: any): Context[] {
     return Array<Context>();
