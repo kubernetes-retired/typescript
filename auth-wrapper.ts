172ab0f3e4cd7a8aa4f7a45b39493e1bc54510ff
Aaron Schlesinger <aaschles@microsoft.com>
2017-07-11 16:15:16 -0700

    Adding auth wrapper for extensions

--- auth-wrapper.ts
+++ auth-wrapper.ts
@@ -12,5 +12,14 @@ export class Core_v1Api extends api.Core_v1Api {
     }
 }
 
+export class Extensions_v1beta1Api extends api.Extensions_v1beta1Api {
+    constructor(baseUri: string) {
+        super(baseUri);
+    }
+    public setDefaultAuthentication(auth: api.Authentication) {
+        this.authentications.default = auth;
+    }
+}
+
 // TODO: Add other API objects here
 
